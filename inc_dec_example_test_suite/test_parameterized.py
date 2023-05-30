77bb9b00c43f20a6b549241503c3c891865e70da
eleanorjboyd <eleanorboyd@microsoft.com>
2023-05-30 09:09:27 -0700

    fix parameterized line

--- inc_dec_example_test_suite/test_parameterized.py
+++ inc_dec_example_test_suite/test_parameterized.py
@@ -1,10 +1,6 @@
 import pytest
 
 
-# Testing pytest with parametrized tests. The first two pass, the third fails.
-# The tests ids are parametrize_tests.py::test_adding[3+5-8] and so on.
-@pytest.mark.parametrize( 
-    "actual, expected", [("3+2", 5), ("2+4", 6), ("6+9", 16)]
-)
-def test_adding(actual, expected):
-    assert eval(actual) == expected
+@pytest.mark.parametrize("num", range(1, 100))
+def test_odd_even(num):
+    assert True
