89617520f9e9031155b3fbfbbf155753c3833e33
eleanorjboyd <eleanorboyd@microsoft.com>
2023-10-23 14:57:46 -0700

    logging test addition

--- inc_dec_example_test_suite/test_logging.py
+++ inc_dec_example_test_suite/test_logging.py
@@ -5,9 +5,11 @@ import sys
 import unittest
 
 
+# Pytest specific test, but pytest will pick up on both tests
 def test_logging2(caplog):
     logger = logging.getLogger(__name__)
-    caplog.set_level(logging.DEBUG)  # Set minimum log level to capture
+    # caplog.set_level(logging.DEBUG)  # Debug level
+    caplog.set_level(logging.ERROR)  # Error level
 
     logger.debug("This is a debug message.")
     logger.info("This is an info message.")
@@ -21,16 +23,18 @@ def test_logging2(caplog):
     assert False
 
 
+# Unittest specific test, unittest will only see this test while pytest will see both tests
 class TestLogger(unittest.TestCase):
     def test_logging(self):
         logger = logging.getLogger(__name__)
         logger.setLevel(logging.DEBUG)  # Set the desired log level
+        # logger.setLevel(logging.ERROR)  # Set the desired log level
 
-        logger.debug("This is a debug message.")
-        logger.info("This is an info message.")
-        logger.warning("This is a warning message.")
-        logger.error("This is an error message.")
-        logger.critical("This is a critical message.")
+        logger.debug("This is a debug message in a unittest.")
+        logger.info("This is an info message in a unittest.")
+        logger.warning("This is a warning message in a unittest.")
+        logger.error("This is an error message in a unittest.")
+        logger.critical("This is a critical message in a unittest.")
 
         # Printing to stdout and stderr
         print("This is a stdout message.")
